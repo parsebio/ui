a06f629746233ac67e2d3412fc26c02362441250
stefanbabukov <stefanbabukov98@gmail.com>
2024-02-09 09:35:42 +0000

    updated secondary analysis page layout
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/pages/secondary-analysis/index.jsx
+++ src/pages/secondary-analysis/index.jsx
@@ -3,17 +3,19 @@ import {
   Modal,
 } from 'antd';
 import Header from 'components/Header';
+import ProjectsListContainer from 'components/data-management/project/ProjectsListContainer';
 import NewSecondaryProject from 'components/secondary-analysis/NewSecondaryProject';
 import SampleLTUpload from 'components/secondary-analysis/SampleLTUpload';
 import SelectReferenceGenome from 'components/secondary-analysis/SelectReferenceGenome';
 import UploadFastQ from 'components/secondary-analysis/UploadFastQ';
 import OverviewMenu from 'components/secondary-analysis/OverviewMenu';
+import MultiTileContainer from 'components/MultiTileContainer';
 
 const SecondaryAnalysis = () => {
   const [currentStep, setCurrentStep] = useState('createProject');
 
   const onNext = () => setCurrentStep(secondaryAnalaysisWizardSteps[currentStep].nextStep);
-  const onCancel = () => setCurrentStep('overviewMenu');
+  const onCancel = () => setCurrentStep(null);
   const secondaryAnalaysisWizardSteps = {
     createProject: {
       render: () => <NewSecondaryProject onNext={onNext} onCancel={onCancel} />,
@@ -33,26 +35,53 @@ const SecondaryAnalysis = () => {
     fastQUpload: {
       render: () => <UploadFastQ onNext={onNext} onCancel={onCancel} />,
       title: 'Fastq files',
-      nextStep: 'overviewMenu',
+      nextStep: null,
     },
+  };
+  const PROJECTS_LIST = 'Runs';
+  const PROJECT_DETAILS = 'Run Details';
+  const TILE_MAP = {
+    [PROJECTS_LIST]: {
+      toolbarControls: [],
+      component: (width, height) => (
+        <ProjectsListContainer
+          height={height}
+          onCreateNewProject={() => setCurrentStep('createProject')}
+        />
+      ),
+    },
+    [PROJECT_DETAILS]: {
+      toolbarControls: [],
+      component: (width, height) => (
+        <OverviewMenu wizardSteps={secondaryAnalaysisWizardSteps} setCurrentStep={setCurrentStep} />
+      ),
+    },
+  };
+  const windows = {
+    direction: 'row',
+    first: PROJECTS_LIST,
+    second: PROJECT_DETAILS,
+    splitPercentage: 23,
   };
 
   return (
     <>
       <Header title='Secondary Analysis' />
-      {currentStep === 'overviewMenu' ? <OverviewMenu wizardSteps={secondaryAnalaysisWizardSteps} setCurrentStep={setCurrentStep} /> : (
-        <Modal
-          title={secondaryAnalaysisWizardSteps[currentStep].title}
-          open
-          onOk={onNext}
-          onCancel={onCancel}
-          okText='Next'
-          cancelButtonProps={{ style: { display: 'none' } }}
-        >
-          {secondaryAnalaysisWizardSteps[currentStep].render()}
-        </Modal>
-      )}
 
+      <Modal
+        open={currentStep}
+        title={secondaryAnalaysisWizardSteps[currentStep]?.title}
+        onOk={onNext}
+        onCancel={onCancel}
+        okText={currentStep === 'createProject' ? 'Create' : 'Next'}
+        cancelButtonProps={{ style: { display: 'none' } }}
+      >
+        {secondaryAnalaysisWizardSteps[currentStep]?.render()}
+      </Modal>
+      <MultiTileContainer
+        tileMap={TILE_MAP}
+        initialArrangement={windows}
+      />
     </>
   );
 };
