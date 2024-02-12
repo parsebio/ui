01c5cdee7c604085581278c5c26d5433a3729864
stefanbabukov <stefanbabukov98@gmail.com>
2024-02-12 12:16:33 +0000

    no dropdown menu for secondary projects
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/data-management/project/ProjectsListContainer.jsx
+++ src/components/data-management/project/ProjectsListContainer.jsx
@@ -1,13 +1,12 @@
+import React, { useState } from 'react';
 import {
   Button,
   Dropdown,
   Menu,
   Space,
 } from 'antd';
-import React, { useState } from 'react';
-import { useAppRouter } from 'utils/AppRouteProvider';
-
 import PropTypes from 'prop-types';
+import { useAppRouter } from 'utils/AppRouteProvider';
 import integrationTestConstants from 'utils/integrationTestConstants';
 import { modules } from 'utils/constants';
 import ProjectsList from './ProjectsList';
@@ -32,26 +31,37 @@ const ProjectsListContainer = (props) => {
     },
   ];
 
+  // Conditionally render button based on projectType
+  const createButton = projectType === 'secondary' ? (
+    <Button
+      data-test-id={integrationTestConstants.ids.CREATE_NEW_PROJECT_BUTTON}
+      type='primary'
+      block
+      onClick={onCreateNewProject}
+    >
+      Create New Run
+    </Button>
+  ) : (
+    <Dropdown
+      overlay={<Menu items={menuItems} />}
+      trigger={['click']}
+      placement='bottomRight'
+    >
+      <Button
+        data-test-id={integrationTestConstants.ids.CREATE_NEW_PROJECT_BUTTON}
+        type='primary'
+        block
+      >
+        Create New Project
+      </Button>
+    </Dropdown>
+  );
+
   return (
     <Space direction='vertical' style={{ width: '100%' }}>
-      <Dropdown
-        menu={{ items: menuItems }}
-        trigger={['click']}
-        placement='bottomRight'
-      >
-        <Button
-          data-test-id={integrationTestConstants.ids.CREATE_NEW_PROJECT_BUTTON}
-          type='primary'
-          block
-        >
-          Create New
-          {' '}
-          {projectType === 'secondary' ? 'Run' : 'Project'}
-        </Button>
-      </Dropdown>
+      {createButton}
       <ProjectSearchBox projectType={projectType} onChange={(searchRegex) => setFilterParam(searchRegex)} />
-      {projectType !== 'secondary' ? <ProjectsList height={height} filter={filterParam} />
-        : <></>}
+      {projectType !== 'secondary' && <ProjectsList height={height} filter={filterParam} />}
     </Space>
   );
 };
