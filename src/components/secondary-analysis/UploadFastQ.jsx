3cde7720472d8bed2d874d906f15533c49c1320b
Oliver Gibson <oliver@biomage.net>
2024-03-05 16:31:23 -0300

    Add expandable

--- src/components/secondary-analysis/UploadFastQ.jsx
+++ src/components/secondary-analysis/UploadFastQ.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import {
   Form, Empty, Divider, List, Space, Typography, Button,
 } from 'antd';
-import { CheckCircleTwoTone, CloseCircleTwoTone, DeleteOutlined } from '@ant-design/icons';
+import {
+  CheckCircleTwoTone, CloseCircleTwoTone, DeleteOutlined, WarningOutlined,
+} from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 import Dropzone from 'react-dropzone';
 import integrationTestConstants from 'utils/integrationTestConstants';
@@ -11,39 +13,58 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 import uploadSecondaryAnalysisFile from 'utils/secondary-analysis/uploadSecondaryAnalysisFile';
 
+import Expandable from 'components/Expandable';
+
 const { Text } = Typography;
 
 const UploadFastQ = (props) => {
   const { secondaryAnalysisId, renderFastqFileTable, setFilesNotUploaded } = props;
   const [filesList, setFilesList] = useState([]);
+  const emptyFiles = { valid: [], invalid: [] };
+  const [files, setFiles] = useState(emptyFiles);
+
   const dispatch = useDispatch();
 
   const beginUpload = async () => {
     // Save all files first and get uploadUrlParams for each
-    const uploadUrlParamsList = await Promise.all(filesList
+    const uploadUrlParamsList = await Promise.all(files.valid
       .map((file) => dispatch(createSecondaryAnalysisFile(secondaryAnalysisId, file, 'fastq'))));
 
     // upload files one by one using the corresponding uploadUrlParams
     await uploadUrlParamsList.reduce(async (promiseChain, uploadUrlParams, index) => {
     // Ensure the previous upload is completed
       await promiseChain;
-      const file = filesList[index];
+      const file = files.valid[index];
       return uploadSecondaryAnalysisFile(file, secondaryAnalysisId, uploadUrlParams, dispatch);
     }, Promise.resolve()); // Start with an initially resolved promise
   };
 
   useEffect(() => {
-    setFilesNotUploaded(Boolean(filesList.length));
-  }, [filesList]);
+    setFilesNotUploaded(Boolean(files.valid.length));
+  }, [files]);
 
-  const onDrop = (files) => {
-    setFilesList(files);
+  const onDrop = (newFiles) => {
+    const filteredFiles = newFiles
+      .filter((file) => !file.name.startsWith('.') && !file.name.startsWith('__MACOSX'))
+      .filter((file) => file.name.endsWith('.fastq') || file.name.endsWith('.fastq.gz'));
+
+    const invalid = _.difference(newFiles, filteredFiles)
+      .map((file) => ({ path: file.path, rejectReason: 'Invalid file path. Check the instructions in the modal for more information' }));
+
+    console.log('FILES');
+    console.log(filteredFiles);
+
+    // Add definition of invalid
+    setFiles({
+      valid: [...files.valid, ...filteredFiles],
+      invalid: [...files.invalid, ...invalid],
+    });
   };
 
   const removeFile = (fileName) => {
-    const newArray = _.cloneDeep(filesList);
+    const newArray = _.cloneDeep(files.valid);
     _.remove(newArray, (file) => file.name === fileName);
-    setFilesList(newArray);
+    setFiles({ valid: newArray, invalid: files.invalid });
   };
   return (
     <Form
@@ -69,6 +90,59 @@ const UploadFastQ = (props) => {
           <a href='https://support.parsebiosciences.com/hc/en-us/articles/20926505533332-Fundamentals-of-Working-with-Parse-Data' target='_blank' rel='noreferrer'>here</a>
 
         </div>
+        {files.invalid.length > 0 && (
+          <div>
+            <Expandable
+              style={{ width: '100%' }}
+              expandedContent={(
+                <>
+                  <Divider orientation='center' style={{ color: 'red', marginBottom: '0' }}>Ignored files</Divider>
+                  <List
+                    dataSource={files.invalid}
+                    size='small'
+                    itemLayout='horizontal'
+                    pagination
+                    renderItem={(file) => (
+                      <List.Item key={file.path} style={{ height: '100%', width: '100%' }}>
+                        <Space style={{ width: 200, justifyContent: 'center' }}>
+                          <CloseCircleTwoTone twoToneColor='#f5222d' />
+                          <div style={{ width: 200 }}>
+                            <Text
+                              ellipsis={{ tooltip: _.trim(file.path, '/') }}
+                            >
+                              {_.trim(file.path, '/')}
+                            </Text>
+                          </div>
+                        </Space>
+                        <Text style={{ width: '100%', marginLeft: '50px' }}>{file.rejectReason}</Text>
+                      </List.Item>
+                    )}
+                  />
+                </>
+              )}
+              collapsedContent={(
+                <center style={{ cursor: 'pointer' }}>
+                  <Divider orientation='center' style={{ color: 'red' }} />
+                  <Text type='danger'>
+                    {' '}
+                    <WarningOutlined />
+                    {' '}
+                  </Text>
+                  <Text>
+                    {files.invalid.length}
+                    {' '}
+                    file
+                    {files.invalid.length > 1 ? 's were' : ' was'}
+                    {' '}
+                    ignored, click to display
+                  </Text>
+                </center>
+              )}
+            />
+            <br />
+          </div>
+        )}
+
         <Dropzone onDrop={onDrop} multiple>
           {({ getRootProps, getInputProps }) => (
             <div
@@ -87,19 +161,19 @@ const UploadFastQ = (props) => {
           type='primary'
           key='create'
           block
-          disabled={!filesList.length}
+          disabled={!files.valid.length}
           onClick={() => {
-            beginUpload(filesList);
-            setFilesList([]);
+            beginUpload(files.valid);
+            setFiles(emptyFiles);
           }}
         >
           Upload
         </Button>
-        {filesList.length > 0 && (
+        {files.valid.length > 0 && (
           <>
             <Divider orientation='center'>To upload</Divider>
             <List
-              dataSource={filesList}
+              dataSource={files.valid}
               size='small'
               itemLayout='horizontal'
               grid='{column: 4}'
