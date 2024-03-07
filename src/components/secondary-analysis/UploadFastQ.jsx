29abbd094b226980e0582b44c299b8065668e467
stefanbabukov <stefanbabukov98@gmail.com>
2024-03-07 11:49:17 +0000

    fix
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/secondary-analysis/UploadFastQ.jsx
+++ src/components/secondary-analysis/UploadFastQ.jsx
@@ -26,19 +26,6 @@ const UploadFastQ = (props) => {
     setFilesNotUploaded(Boolean(fileHandlesList.length));
   }, [fileHandlesList]);
 
-  const getAllFilesFromDirectory = async (directoryHandle) => {
-    const files = [];
-    for await (const entry of directoryHandle.values()) {
-      if (entry.kind === 'file') {
-        files.push(entry);
-      } else if (entry.kind === 'directory') {
-        const nestedFiles = await getAllFilesFromDirectory(entry);
-        files.push(...nestedFiles);
-      }
-    }
-    return files;
-  };
-
   const handleFileSelection = async () => {
     try {
       const opts = { multiple: true };
