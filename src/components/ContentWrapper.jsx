26147d402ba620400c0ca6d761836a44f7e54da3
cosa65 <martin@biomage.net>
2024-03-08 11:06:42 -0300

    Add getDisabled
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -112,6 +112,11 @@ const ContentWrapper = (props) => {
     status: backendStatus,
   } = useSelector(getBackendStatus(currentExperimentId));
 
+  const { activeSecondaryAnalysisId } = useSelector((state) => state.secondaryAnalyses.meta);
+  const { loading: analysisStatusLoading, current: analysisStatus } = useSelector(
+    (state) => state.secondaryAnalyses[activeSecondaryAnalysisId]?.status ?? {},
+  );
+
   const qcStatusKey = backendStatus?.pipeline?.status;
   const qcRunning = qcStatusKey === 'RUNNING';
   const qcRunningError = backendErrors.includes(qcStatusKey);
@@ -335,6 +340,10 @@ const ContentWrapper = (props) => {
       module: modules.SECONDARY_ANALYSIS,
       icon: <CodeSandboxOutlined />,
       name: 'Secondary Analysis',
+      getDisabled: () => (
+        analysisStatus
+        && currentModule === modules.SECONDARY_ANALYSIS_OUTPUT && analysisStatus === 'running'
+      ),
       disableIfNoExperiment: false,
       disabledByPipelineStatus: true,
       disabledIfSeuratComplete: false,
@@ -343,6 +352,10 @@ const ContentWrapper = (props) => {
       module: modules.SECONDARY_ANALYSIS_OUTPUT,
       icon: <CodeSandboxOutlined />,
       name: 'Pipeline Output',
+      getDisabled: () => (
+        analysisStatus
+        && currentModule === modules.SECONDARY_ANALYSIS && analysisStatus === 'not_created'
+      ),
       disableIfNoExperiment: false,
       disabledByPipelineStatus: true,
       disabledIfSeuratComplete: false,
@@ -421,7 +434,13 @@ const ContentWrapper = (props) => {
   };
 
   const menuItemRender = ({
-    module, icon, name, disableIfNoExperiment, disabledByPipelineStatus, disabledIfSeuratComplete,
+    module,
+    icon,
+    name,
+    disableIfNoExperiment,
+    disabledByPipelineStatus,
+    disabledIfSeuratComplete,
+    getDisabled = () => false,
   }) => {
     const needRerunPipeline = pipelinesRerunStatus === null || pipelinesRerunStatus.rerun;
 
@@ -449,7 +468,7 @@ const ContentWrapper = (props) => {
       icon,
       label: name,
       disabled: notProcessedExperimentDisable || pipelineStatusDisable
-        || seuratCompleteDisable || nonExperimentModule,
+        || seuratCompleteDisable || nonExperimentModule || getDisabled(),
       onClick: () => navigateTo(
         module,
         { experimentId: currentExperimentId, analysisId: currentAnalysisIdRef.current },
