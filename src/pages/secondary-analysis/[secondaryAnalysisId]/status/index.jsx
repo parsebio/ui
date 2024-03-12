0357e515f7b8cb7c33fde129d23f303e5eeb7106
cosa65 <martin@biomage.net>
2024-03-12 15:38:55 -0300

    Remove sandbox
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/pages/secondary-analysis/[secondaryAnalysisId]/status/index.jsx
+++ src/pages/secondary-analysis/[secondaryAnalysisId]/status/index.jsx
@@ -144,7 +144,7 @@ const AnalysisDetails = ({ secondaryAnalysisId }) => {
         {renderDownloadAllOutputsButton()}
       </Space>
 
-      <iframe sandbox src={URL.createObjectURL(reports[selectedReport])} title='My Document' style={{ height: '100%', width: '100%' }} />
+      <iframe src={URL.createObjectURL(reports[selectedReport])} title='My Document' style={{ height: '100%', width: '100%' }} />
     </>
 
   );
