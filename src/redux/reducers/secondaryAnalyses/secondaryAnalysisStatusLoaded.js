3f39031f125b4a4f569b3d2a673892d875c0db40
stefanbabukov <stefanbabukov98@gmail.com>
2024-03-12 15:43:52 +0000

    fix
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/redux/reducers/secondaryAnalyses/secondaryAnalysisStatusLoaded.js
+++ src/redux/reducers/secondaryAnalyses/secondaryAnalysisStatusLoaded.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-param-reassign */
 import produce from 'immer';
 
-const secondaryAnalysesLoaded = produce((draft, action) => {
+const secondaryAnalysisStatusLoaded = produce((draft, action) => {
   const { secondaryAnalysisId, status } = action.payload;
-  if (status) {
-    draft[secondaryAnalysisId].status.current = status;
-  }
+  draft[secondaryAnalysisId].status.current = status;
   draft[secondaryAnalysisId].status.loading = false;
 });
 
-export default secondaryAnalysesLoaded;
+export default secondaryAnalysisStatusLoaded;
