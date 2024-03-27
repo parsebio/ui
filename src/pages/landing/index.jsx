8cab40d628a68deedc153194a013ee86641e2eaa
stefanbabukov <stefanbabukov98@gmail.com>
2024-03-27 13:11:07 +0200

    styling change to landing page
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -25,51 +25,52 @@ const LandingPage = () => {
   if (shouldRenderLandingPage) {
     return (
       <div style={{
-        display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+        display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%',
       }}
       >
-        <Card
-          size='large'
-          style={{
-            width: '80%',
-            maxWidth: '800px',
-          }}
-          bordered
-        >
-          <Space direction='vertical'>
-            <Title level={3}>
-              I am a Parse Biosciences customer and I have
-              Fastq files from a Parse Biosciences kit.
-            </Title>
-            <Space direction='horizontal'>
-              <p>
-                The Pipeline module is where you can upload your Parse
-                Biosciences Fastq files for alignment to
-                your selected genome. The Pipeline module outputs
-                include a Quality Control report, downloadable
-                count matrices and automated integration with downstream Insights module.
-              </p>
-              <Button size='large' type='primary' onClick={() => navigateTo(modules.SECONDARY_ANALYSIS)}>Take me to Pipeline</Button>
+        <Space direction='vertical'>
+          <Title level={2}>Welcome to the Parse Biosciences cloud analysis platform!</Title>
+          <Card
+            style={{
+              width: '100%',
+              maxWidth: '900px',
+            }}
+            bordered
+          >
+            <Space direction='vertical' size='large'>
+              <Title level={3}>
+                I am a Parse Biosciences customer and I have
+                Fastq files from a Parse Biosciences kit.
+              </Title>
+              <Space direction='horizontal'>
+                <p>
+                  The Pipeline module is where you can upload your Parse
+                  Biosciences Fastq files for alignment to
+                  your selected genome. The Pipeline module outputs
+                  include a Quality Control report, downloadable
+                  count matrices and automated integration with downstream Insights module.
+                </p>
+                <Button size='large' type='primary' onClick={() => navigateTo(modules.SECONDARY_ANALYSIS)}>Take me to Pipeline</Button>
+              </Space>
+              <Divider orientation='center'>OR</Divider>
+              <Title level={3}>
+                I have processed data files, such as Pipeline output
+                files, count matrices or a Seurat object.
+              </Title>
+              <Space direction='horizontal'>
+                <p>
+                  The Insights module is where you can upload files that
+                  have already been pre-processed. Supported
+                  file types include: output files from the Pipeline module,
+                  count matrices of multiple technology
+                  types, H5 files, and Seurat objects. Explore your data and generate publication-ready
+                  figures with Insights!
+                </p>
+                <Button size='large' type='primary' onClick={() => navigateTo(modules.DATA_MANAGEMENT)}>Take me to Insights</Button>
+              </Space>
             </Space>
-            <Divider orientation='center'>OR</Divider>
-            <Title level={3}>
-              I have processed data files, such as Pipeline output
-              files, count matrices or a Seurat object.
-            </Title>
-            <Space direction='horizontal'>
-              <p>
-                The Insights module is where you can upload files that
-                have already been pre-processed. Supported
-                file types include: output files from the Pipeline module,
-                count matrices of multiple technology
-                types, H5 files, and Seurat objects. Explore your data and generate publication-ready
-                figures with Insights!
-              </p>
-              <Button size='large' type='primary' onClick={() => navigateTo(modules.DATA_MANAGEMENT)}>Take me to Insights</Button>
-            </Space>
-          </Space>
-
-        </Card>
+          </Card>
+        </Space>
       </div>
     );
   }
