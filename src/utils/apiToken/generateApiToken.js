fcb046d99b8a246b543158763135b1f739aa4a85
cosa65 <martin@biomage.net>
2024-04-08 11:45:58 -0300

    Remove 'replace' bool from post body
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/utils/apiToken/generateApiToken.js
+++ src/utils/apiToken/generateApiToken.js
@@ -1,12 +1,11 @@
 import fetchAPI from '../http/fetchAPI';
 
-const generateApiToken = async (replace) => (
+const generateApiToken = async () => (
   await fetchAPI('/v2/cliUpload/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ replace }),
   })
 );
 
