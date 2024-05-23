474dc86bb2d033b1eac3def4c2890da56e45b1ca
stefanbabukov <stefanbabukov98@gmail.com>
2024-05-23 20:22:40 +0100

    only first filter dropdown open by default
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/SingleComponentMultipleDataContainer.jsx
+++ src/components/SingleComponentMultipleDataContainer.jsx
@@ -1,50 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Collapse } from 'antd';
-import { FixedSizeList as List } from 'react-window';
 
 const { Panel } = Collapse;
 
 const SingleComponentMultipleDataContainer = (props) => {
   const {
-    defaultActiveKey, inputsList, baseComponentRenderer,
+    inputsList, baseComponentRenderer,
   } = props;
 
-  const Row = ({ index, style }) => {
-    const { key, headerName, params } = inputsList[index];
-    return (
-      <div style={style} key={key}>
-        <Collapse defaultActiveKey={defaultActiveKey}>
+  const defaultActiveKey = inputsList.length > 0 ? [inputsList[0].key] : [];
+
+  return (
+    <Collapse defaultActiveKey={defaultActiveKey}>
+      {
+        inputsList.map(({ key, headerName, params }) => (
           <Panel header={headerName} key={key}>
             {baseComponentRenderer(params)}
           </Panel>
-        </Collapse>
-      </div>
-    );
-  };
-
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <List
-        height={window.innerHeight} // or any fixed height you prefer
-        itemCount={inputsList.length}
-        itemSize={600}
-        width='100%'
-      >
-        {Row}
-      </List>
-    </div>
+        ))
+      }
+    </Collapse>
   );
 };
 
 SingleComponentMultipleDataContainer.propTypes = {
-  defaultActiveKey: PropTypes.array,
   inputsList: PropTypes.array,
   baseComponentRenderer: PropTypes.func.isRequired,
 };
 
 SingleComponentMultipleDataContainer.defaultProps = {
-  defaultActiveKey: '',
   inputsList: [],
 };
 
