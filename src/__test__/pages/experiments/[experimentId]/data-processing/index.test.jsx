71b942d3bc2ca9a91e18731c71ee34a7097f2b7a
cosa65 <martin@biomage.net>
2024-06-12 15:36:38 -0300

    Update tst
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/__test__/pages/experiments/[experimentId]/data-processing/index.test.jsx
+++ src/__test__/pages/experiments/[experimentId]/data-processing/index.test.jsx
@@ -14,7 +14,7 @@ import { initialPlotConfigStates } from 'redux/reducers/componentConfig/initialS
 import initialCellSetsState from 'redux/reducers/cellSets/initialState';
 import initialSamplesState, { sampleTemplate } from 'redux/reducers/samples/initialState';
 
-import { getBackendStatus } from 'redux/selectors';
+import { getBackendStatus, getChangedStepKeys } from 'redux/selectors';
 import '__test__/test-utils/setupTests';
 
 import { runQC } from 'redux/actions/pipeline';
@@ -79,6 +79,8 @@ getBackendStatus.mockImplementation(() => () => ({
     },
   },
 }));
+
+getChangedStepKeys.mockImplementation(() => () => (new Set([])));
 
 const getStore = (experimentId, settings = {}) => {
   const initialState = {
