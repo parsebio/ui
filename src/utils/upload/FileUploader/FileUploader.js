ce6705c5e06c0df3018587a9b291982bc0000398
cosa65 <martin@biomage.net>
2024-06-18 15:16:35 -0300

    Merge conflicts
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/utils/upload/FileUploader/FileUploader.js
+++ src/utils/upload/FileUploader/FileUploader.js
@@ -1,10 +1,6 @@
-/* eslint-disable no-unused-expressions */
-import _ from 'lodash';
-
 import { AsyncGzip } from 'fflate';
 import filereaderStream from 'filereader-stream';
 
-import UploadStatus from 'utils/upload/UploadStatus';
 import PartUploader from 'utils/upload/FileUploader/PartUploader';
 import FileUploaderError from 'utils/errors/upload/FileUploaderError';
 import fetchAPI from 'utils/http/fetchAPI';
@@ -146,14 +142,6 @@ class FileUploader {
     });
   }
 
-  #createOnUploadProgress = (partNumber) => (progress) => {
-    // partNumbers are 1-indexed, so we need to subtract 1 for the array index
-    this.uploadedPartPercentages[partNumber - 1] = progress.progress;
-
-    const percentage = (_.mean(this.uploadedPartPercentages) * 100).toFixed(2);
-    this.onStatusUpdate(UploadStatus.UPLOADING, parseFloat(percentage));
-  };
-
   #setupGzipStreamHandlers = () => {
     this.gzipStream.ondata = async (err, chunk) => {
       try {
@@ -231,59 +219,42 @@ class FileUploader {
     this.reject(new FileUploaderError(reasonMessage));
   }
 
-  // <<<<<<< HEAD:src/utils/upload/FileUploader/FileUploader.js
   #handleChunkLoadFinished = async (chunk) => {
+    // This assigns a part number to each chunk that arrives
+    // They are read in order, so it should be safe
+    this.partNumberIt += 1;
     try {
-      const onUploadProgress = this.#createOnUploadProgress(this.pendingChunks);
+      // const onUploadProgress = this.#createOnUploadProgress(this.pendingChunks);
+      const onUploadProgress = () => [];
       await this.partUploader.uploadChunk(chunk, onUploadProgress);
+
+      // To track when all chunks have been uploaded
+      this.pendingChunks -= 1;
+
+      if (this.pendingChunks > 0) {
+        await this.#releaseUploadSlot();
+      }
+
+      if (this.pendingChunks === 0) {
+        // S3 expects parts to be sorted by number
+        this.uploadedParts.sort(({ PartNumber: PartNumber1 }, { PartNumber: PartNumber2 }) => {
+          if (PartNumber1 === PartNumber2) throw new Error('Non-unique partNumbers found, each number should be unique');
+
+          return PartNumber1 > PartNumber2 ? 1 : -1;
+        });
+
+        this.resolve(this.uploadedParts);
+      }
     } catch (e) {
-      this.#cancelExecution(UploadStatus.UPLOAD_ERROR, e);
-    }
-
-    // To track when all chunks have been uploaded
-    this.pendingChunks -= 1;
-
-    if (this.pendingChunks === 0) {
-      const uploadedParts = await this.partUploader.finishUpload();
-
-      this.resolve(uploadedParts);
+      this.#abortUpload(e);
     }
   }
-
-  // #handleChunkLoadFinished = async (chunk) => {
-  //     // This assigns a part number to each chunk that arrives
-  //     // They are read in order, so it should be safe
-  //     this.partNumberIt += 1;
-  //     try {
-  //       await this.#uploadChunk(chunk, this.partNumberIt);
-
-  //       // To track when all chunks have been uploaded
-  //       this.pendingChunks -= 1;
-
-  //       if (this.pendingChunks > 0) {
-  //         await this.#releaseUploadSlot();
-  //       }
-
-  //       if (this.pendingChunks === 0) {
-  //         // S3 expects parts to be sorted by number
-  //         this.uploadedParts.sort(({ PartNumber: PartNumber1 }, { PartNumber: PartNumber2 }) => {
-  //           if (PartNumber1 === PartNumber2) throw new Error('Non-unique partNumbers found, each number should be unique');
-
-  //           return PartNumber1 > PartNumber2 ? 1 : -1;
-  //         });
-
-  //         this.resolve(this.uploadedParts);
-  //       }
-  //     } catch (e) {
-  //       this.#abortUpload(e);
-  //     }
-  //   }
 
   //   #createOnUploadProgress = (chunkNumber) => (progress) => {
   //     // partNumbers are 1-indexed, so we need to subtract 1 for the array index
   //     this.uploadedPartPercentages[chunkNumber - 1] = progress.progress;
 
-  //     const percentage = _.mean(this.uploadedPartPercentages) * 100;
+  //     const percentage = (_.mean(this.uploadedPartPercentages) * 100).toFixed(2);
   //     this.onStatusUpdate(UploadStatus.UPLOADING, Math.floor(percentage));
   //   };
 
