b2f7c866a216b61797d57cf8533d7a317f2c162f
Oliver Gibson <oliver@biomage.net>
2024-06-30 23:09:47 -0300

    Fix error messages

--- src/components/data-processing/FilterResultTable.jsx
+++ src/components/data-processing/FilterResultTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import pipelineWarningUserMessages from 'utils/pipelineErrorUserMessages';
+import pipelineWarningUserMessages from 'utils/pipelineWarningUserMessages';
 
 import { Table, Empty, Alert } from 'antd';
 
--- src/utils/pipelineWarningUserMessages.js
+++ src/utils/pipelineWarningUserMessages.js
@@ -1,3 +1,3 @@
 export default {
-  WARNING_NOT_ENOUGH_CELLS_SCDBLFINDER: '\u26A0\uFE0F There are not enough cells to compute doublet scores. This step will be disabled. Verify filtering in previous steps to make sure no overfiltering is occurring.',
+  WARNING_NOT_ENOUGH_CELLS_SCDBLFINDER: '\u26A0\uFE0F There are not enough cells to compute doublet scores. No barcodes will be filtered out. Double check the previous steps to make sure no overfiltering is occurring.',
 };
