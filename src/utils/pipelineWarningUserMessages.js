09cfa74817d57a846281d1a4fa519502c52fc56d
Oliver Gibson <oliver@biomage.net>
2024-07-01 15:55:05 -0300

    Change warning name for clarity

--- src/utils/pipelineWarningUserMessages.js
+++ src/utils/pipelineWarningUserMessages.js
@@ -1,3 +1,3 @@
 export default {
-  WARNING_NOT_ENOUGH_CELLS_SCDBLFINDER: '\u26A0\uFE0F There are not enough cells to compute doublet scores. No barcodes will be filtered out. Double check the previous steps to make sure no overfiltering is occurring.',
+  WARNING_SCDBLFINDER_NOT_ENOUGH_CELLS: '\u26A0\uFE0F There are not enough cells to compute doublet scores. No barcodes will be filtered out. Double check the previous steps to make sure no overfiltering is occurring.',
 };
