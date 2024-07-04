135857228afc9d68de34762620462e39dc45dbde
stefanbabukov <stefanbabukov98@gmail.com>
2024-07-04 13:48:31 +0100

    test test
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/__test__/pages/pipeline/index.test.jsx
+++ src/__test__/pages/pipeline/index.test.jsx
@@ -50,12 +50,80 @@ const initialState = {
       activeSecondaryAnalysisId: 'analysis-1',
     },
     'analysis-1': {
+      name: 'some analysis',
+      id: 'analysis-1',
       status: {
         current: 'not_created',
         loading: true,
       },
       files: {
-        data: null,
+        data: {
+          '07286c9c-9392-4591-8b1c-c47b355e1108': {
+            id: '07286c9c-9392-4591-8b1c-c47b355e1108',
+            name: 'S1_R1.fastq.gz',
+            size: '70154428',
+            type: 'fastq',
+            upload: {
+              status: {
+                current: 'uploaded',
+                loading: false,
+              },
+            },
+            createdAt: '2024-07-02 16:00:15.08919+00',
+          },
+          '1be1689c-0909-496b-bcf9-c29abb3733cf': {
+            id: '1be1689c-0909-496b-bcf9-c29abb3733cf',
+            name: 'S1_R2.fastq.gz',
+            size: '55879599',
+            type: 'fastq',
+            upload: {
+              status: {
+                current: 'uploaded',
+                loading: false,
+              },
+            },
+            createdAt: '2024-07-02 16:00:16.941129+00',
+          },
+          '0dc7d6ec-b702-4841-b49a-28ad9f0d29a1': {
+            id: '0dc7d6ec-b702-4841-b49a-28ad9f0d29a1',
+            name: 'S2_R1.fastq.gz',
+            size: '70151802',
+            type: 'fastq',
+            upload: {
+              status: {
+                current: 'uploaded',
+                loading: false,
+              },
+            },
+            createdAt: '2024-07-02 16:00:18.312718+00',
+          },
+          'e8040b41-509f-473d-aafe-92d1c27ab2e3': {
+            id: 'e8040b41-509f-473d-aafe-92d1c27ab2e3',
+            name: 'S2_R2.fastq.gz',
+            size: '55879479',
+            type: 'fastq',
+            upload: {
+              status: {
+                current: 'uploaded',
+                loading: false,
+              },
+            },
+            createdAt: '2024-07-02 16:00:20.056015+00',
+          },
+          sampleltfile: {
+            id: 'smaplelt',
+            name: 'samplelt.xlsm',
+            upload: {
+              status: {
+                current: 'uploaded',
+                loading: false,
+              },
+            },
+            type: 'samplelt',
+          },
+        },
+        loading: false,
+        error: false,
       },
     },
   },
@@ -89,48 +157,101 @@ describe('Pipeline Page', () => {
     </DndProvider>,
   );
 
-  // it('renders loading state initially', () => {
-  //   renderPipelinePage();
-  //   expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
-  // });
+  it('renders the Pipeline page', () => {
+    renderPipelinePage();
 
-  // it('loads secondary analyses on mount', async () => {
-  //   renderPipelinePage();
-  //   await waitFor(() => {
-  //     expect(loadSecondaryAnalyses).toHaveBeenCalled();
-  //   });
-  // });
+    expect(screen.getByText(/Run ID/i)).toBeInTheDocument();
+  });
 
-  // it('loads secondary analysis files and status when activeSecondaryAnalysisId is set', async () => {
-  //   renderPipelinePage();
-  //   await waitFor(() => {
-  //     expect(loadSecondaryAnalysisFiles).toHaveBeenCalledWith('analysis-1');
-  //     expect(loadSecondaryAnalysisStatus).toHaveBeenCalledWith('analysis-1');
-  //   });
-  // });
+  it('uploads files correctly', async () => {
+    renderPipelinePage();
 
-  // it('renders the main screen details correctly', async () => {
-  //   renderPipelinePage();
+    fireEvent.click(screen.getByText(/Upload your sample loading table/i));
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText(/Sample File/i)).toBeInTheDocument();
-  //     expect(screen.getByText(/Fastq File/i)).toBeInTheDocument();
-  //   });
-  // });
+    const uploadInput = screen.getByLabelText(/Upload/i);
 
-  // it('renders the correct buttons and handles clicks', async () => {
-  //   renderPipelinePage();
+    const file = new File(['sample data'], 'sample.lt', {
+      type: 'text/plain',
+    });
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText(/Run the pipeline/i)).toBeInTheDocument();
-  //   });
+    fireEvent.change(uploadInput, {
+      target: { files: [file] },
+    });
 
-  //   fireEvent.click(screen.getByText(/Run the pipeline/i));
+    await waitFor(() => {
+      expect(screen.getByText(/Sample File/i)).toBeInTheDocument();
+    });
+  });
 
-  //   await waitFor(() => {
-  //     expect(launchSecondaryAnalysis).toHaveBeenCalledWith('analysis-1');
-  //   });
-  // });
+  it('launches the analysis', async () => {
+    const { navigateTo } = useAppRouter();
+
+    const mockState = {
+      ...initialState,
+      secondaryAnalyses: {
+        ...initialState.secondaryAnalyses,
+        'analysis-1': {
+          ...initialState.secondaryAnalyses['analysis-1'],
+          status: {
+            loading: false,
+            shouldRerun: false,
+            current: 'not_created',
+          },
+        },
+      },
+    };
+
+    const newStore = mockStore(mockState);
+
+    renderPipelinePage(newStore);
+
+    fireEvent.click(screen.getByRole('button', { name: /Run the pipeline/i }));
+
+    await waitFor(() => {
+      expect(navigateTo).toHaveBeenCalledWith(modules.SECONDARY_ANALYSIS_OUTPUT, { secondaryAnalysisId: 'analysis-1' });
+    });
+  });
+
+  it('updates secondary analysis details', async () => {
+    const mockState = {
+      ...initialState,
+      secondaryAnalyses: {
+        ...initialState.secondaryAnalyses,
+        'analysis-1': {
+          ...initialState.secondaryAnalyses['analysis-1'],
+          status: {
+            loading: false,
+            shouldRerun: false,
+            current: 'not_created',
+          },
+        },
+      },
+    };
+
+    const newStore = mockStore(mockState);
+
+    renderPipelinePage(newStore);
+
+    fireEvent.click(screen.getByText(/Provide the details of the experimental setup/i));
+
+    const input = screen.getByPlaceholderText(/Enter details/i);
+
+    fireEvent.change(input, {
+      target: { value: 'New Analysis Details' },
+    });
+
+    fireEvent.click(screen.getByRole('button', { name: /Next/i }));
+
+    await waitFor(() => {
+      expect(newStore.getActions()).toContainEqual({
+        type: 'UPDATE_SECONDARY_ANALYSIS',
+        payload: {
+          id: 'analysis-1',
+          details: { description: 'New Analysis Details' },
+        },
+      });
+    });
+  });
 
   it('navigates to the correct module on button click', async () => {
     const { navigateTo } = useAppRouter();
@@ -155,7 +276,7 @@ describe('Pipeline Page', () => {
 
     renderPipelinePage(newStore);
 
-    fireEvent.click(screen.getByText(/Go to output/i));
+    fireEvent.click(screen.getByRole('button', { name: /Go to output/i }));
 
     await waitFor(() => {
       expect(navigateTo).toHaveBeenCalledWith(modules.SECONDARY_ANALYSIS_OUTPUT, { secondaryAnalysisId: 'analysis-1' }, false, true);
