0b97f100f23d5322876919107c2f01ec45225fc9
cosa65 <martin@biomage.net>
2024-07-25 12:00:51 -0300

    Update test
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/__test__/components/data-exploration/differential-expression-tool/DiffExprResults.test.jsx
+++ src/__test__/components/data-exploration/differential-expression-tool/DiffExprResults.test.jsx
@@ -92,9 +92,9 @@ const resultState = {
       type: 'between',
       group: {
         between: {
-          cellSet: 'louvain/cluster-a',
-          compareWith: 'louvain/cluster-b',
-          basis: 'scratchpad/scratchpad-a',
+          cellSet: 'cluster-a',
+          compareWith: 'cluster-b',
+          basis: 'scratchpad-a',
         },
       },
       advancedFilters: [],
