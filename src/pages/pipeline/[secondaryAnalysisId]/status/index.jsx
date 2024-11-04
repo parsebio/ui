8301a97bd2a975366c9bd7a5bcdf037db66a261f
Oliver Gibson <oliver@biomage.net>
2024-11-04 10:39:28 -0300

    Fix

--- src/pages/pipeline/[secondaryAnalysisId]/status/index.jsx
+++ src/pages/pipeline/[secondaryAnalysisId]/status/index.jsx
@@ -264,19 +264,24 @@ const AnalysisDetails = ({ secondaryAnalysisId }) => {
               </Text>
               {renderDownloadLogsButton()}
 
-              <Text type='secondary' style={{ marginTop: '8px', whiteSpace: 'normal', overflowWrap: 'break-word' }}>
+              <Text type='secondary' style={{ marginTop: '8px' }}>
                 For help addressing your pipeline failure, visit our article
+                {' '}
                 <a href='https://support.parsebiosciences.com/hc/en-us/articles/31579430276372-How-to-troubleshoot-Pipeline-failures-in-Trailmaker' target='_blank' rel='noopener noreferrer'>
                   How to troubleshoot pipeline failures in Trailmaker
                 </a>
-                . To access the article, make sure you are logged into the
-                support suite using your Parse Biosciences account.
+                .
+                <br />
+                To access the article, make sure you are logged into the support suite
+                <br />
+                using your Parse Biosciences account.
               </Text>
 
               <Text type='secondary' style={{ marginTop: '8px' }}>
                 If your error message is not covered in the article or you need further support,
                 <br />
                 contact us at
+                {' '}
                 <a href='mailto:support@parsebiosciences.com'>support@parsebiosciences.com</a>
                 .
               </Text>
