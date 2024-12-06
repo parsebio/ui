6693bd0cd925493bc2fb22c375fbd9f33af7a972
stefanbabukov <stefanbabukov98@gmail.com>
2024-12-06 12:39:45 +0000

    better check
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/redux/selectors/cellSets/getCellSetsHierarchyByKeys.js
+++ src/redux/selectors/cellSets/getCellSetsHierarchyByKeys.js
@@ -3,7 +3,7 @@ import createMemoizedSelector from 'redux/selectors/createMemoizedSelector';
 import getCellSetsHierarchy from './getCellSetsHierarchy';
 
 const getCellSetsHierarchyByKeys = (keys) => (hierarchy) => {
-  if (hierarchy.length === 0 || keys?.length === 0) return [];
+  if (hierarchy.length === 0 || !keys?.length > 0) return [];
 
   return (
     keys.map((key) => hierarchy.find((child) => child.key === key))
