6bd6b82f252f8a4e8c8b543a1be143c3325fa327
cosa65 <martin@biomage.net>
2025-02-24 09:50:16 -0300

    Add caching with invalidation when cell sets get reloaded
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/webworkers/cellSets/CellSetsWorker.js
+++ src/webworkers/cellSets/CellSetsWorker.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import hash from 'object-hash';
 
 class CellSetsWorker {
   constructor() {
@@ -6,9 +7,15 @@ class CellSetsWorker {
       new URL('./worker.js', import.meta.url),
     );
 
+    this.cellSetsStored = false;
+
     // TODO expand this to support subscribers to different tasks
     // once we have more tasks
     this.taskSubscriber = {};
+
+    this.taskCache = {
+      countCells: {},
+    };
 
     this.activeIdByTask = {
       countCells: null,
@@ -23,15 +30,23 @@ class CellSetsWorker {
         this.taskSubscriber[id].reject(error);
       }
 
-      if (this.activeIdByTask[task] === id) {
-        this.taskSubscriber[id].resolve(payload);
-      }
+      if (task === 'storeCellSets') {
+        this.cellSetsStored = true;
+      } else if (task === 'cellSetCreated') {
+        this.taskSubscriber[id].resolve();
+      } else if (task === 'countCells') {
+        if (this.activeIdByTask.countCells === id) {
+          this.taskSubscriber[id].resolve(payload);
+        }
 
-      delete this.taskSubscriber[id];
+        delete this.taskSubscriber[id];
+      }
     };
 
     this.worker.onerror = (error) => {
       // An unhandled error took place, reject everything because we don't know what failed
+      console.error(error);
+
       Object.keys(this.taskSubscriber).forEach((id) => {
         this.taskSubscriber[id].reject(error);
         delete this.taskSubscriber[id];
@@ -47,11 +62,9 @@ class CellSetsWorker {
     return CellSetsWorker.instance;
   }
 
-  async cellSetCreated(cellSet) {
+  cellSetCreated(cellSet) {
     return new Promise((resolve, reject) => {
       const id = uuidv4();
-
-      this.activeIdByTask.cellSetCreated = id;
       this.taskSubscriber[id] = { resolve, reject };
 
       this.worker.postMessage({
@@ -64,29 +77,40 @@ class CellSetsWorker {
     });
   }
 
-  async storeCellSets(arrayBuffer) {
-    return new Promise((resolve, reject) => {
-      const id = uuidv4();
+  storeCellSets(arrayBuffer) {
+    const id = uuidv4();
 
-      this.activeIdByTask.storeCellSets = id;
-      this.taskSubscriber[id] = { resolve, reject };
+    this.worker.postMessage({
+      id,
+      task: 'storeCellSets',
+      payload: {
+        cellSetsData: arrayBuffer,
+      },
+    }, [arrayBuffer]);
 
-      this.worker.postMessage({
-        id,
-        task: 'storeCellSets',
-        payload: {
-          cellSetsData: arrayBuffer,
-        },
-      }, [arrayBuffer]);
-    });
+    this.invalidateCache();
   }
 
   async countCells(cellSetKeys) {
     return new Promise((resolve, reject) => {
+      const inputHash = hash.MD5([...cellSetKeys].sort());
+      const cachedResult = this.taskCache.countCells[inputHash];
+      if (cachedResult) {
+        resolve(cachedResult);
+        return;
+      }
+
       const id = uuidv4();
 
+      this.taskSubscriber[id] = {
+        resolve: (result) => {
+          this.taskCache.countCells[inputHash] = result;
+          return resolve(result);
+        },
+        reject,
+      };
+
       this.activeIdByTask.countCells = id;
-      this.taskSubscriber[id] = { resolve, reject };
 
       this.worker.postMessage({
         id,
@@ -95,6 +119,12 @@ class CellSetsWorker {
           cellSetKeys,
         },
       });
+    });
+  }
+
+  invalidateCache() {
+    Object.keys(this.taskCache).forEach((task) => {
+      this.taskCache[task] = {};
     });
   }
 }
