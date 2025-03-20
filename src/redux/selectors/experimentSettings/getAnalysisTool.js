2d70293bd7de6ccc44dbf1a33488ad86b430e60f
cosa65 <martin@biomage.net>
2025-03-20 10:12:24 -0300

    Fix
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/redux/selectors/experimentSettings/getAnalysisTool.js
+++ src/redux/selectors/experimentSettings/getAnalysisTool.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import createMemoizedSelector from 'redux/selectors/createMemoizedSelector';
 
 const getAnalysisTool = () => (state) => {
-  const analysisTool = state.processing?.dataIntegration?.analysisTool;
+  const analysisTool = state?.processing?.dataIntegration?.analysisTool;
 
   if (_.isNil(analysisTool)) {
     return null;
