c4ba6e8c483279982da54a8870abfeb8e9781836
stefanbabukov <stefanbabukov98@gmail.com>
2025-04-03 11:43:25 +0100

    invite as owner
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/data-management/project/ShareProjectModal.jsx
+++ src/components/data-management/project/ShareProjectModal.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { UserAddOutlined } from '@ant-design/icons';
 import {
-  Modal, Button, Space, Row, Col, Card, Avatar, Select, Typography,
+  Modal, Button, Space, Row, Col, Card, Avatar, Select, Typography, Popconfirm,
 } from 'antd';
 import { Auth } from '@aws-amplify/auth';
+import { loadExperiments } from 'redux/actions/experiments';
 import loadRoles from 'utils/data-management/experimentSharing/loadRoles';
 import sendInvites from 'utils/data-management/experimentSharing/sendInvites';
 import revokeRole from 'utils/data-management/experimentSharing/revokeRole';
@@ -12,17 +14,27 @@ import revokeRole from 'utils/data-management/experimentSharing/revokeRole';
 const { Text } = Typography;
 
 const ShareProjectModal = (props) => {
+  const dispatch = useDispatch();
   const { onCancel, project, explorerInfoText } = props;
   const [usersWithAccess, setUsersWithAccess] = useState([]);
   const [addedUsers, setAddedUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
   const [role, setRole] = useState('explorer');
+  const [canTransferOwnership, setCanTransferOwnership] = useState(false);
 
   const fetchRoles = async () => {
     const getCurrentUser = await Auth.currentAuthenticatedUser();
     setCurrentUser(getCurrentUser.attributes.email);
 
     const userRole = await loadRoles(project.id);
+    const currentUserRole = userRole.find((user) => user.email === getCurrentUser.attributes.email);
+
+    // if the current user is not in the list of roles, it could mean that its an admin user
+    // the actual admin user check is done in the backend
+    if (currentUserRole.role === 'owner' || getCurrentUser.attributes.email.includes('+admin@parsebiosciences.com')) {
+      setCanTransferOwnership(true);
+    }
+
     setUsersWithAccess(userRole);
   };
 
@@ -38,13 +50,19 @@ const ShareProjectModal = (props) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       );
     if (!isEmailInvalid) {
-      setAddedUsers(selectedUsers);
+      if (role === 'owner') {
+        // Only allow one email when the owner role is selected
+        setAddedUsers([newUser]);
+      } else {
+        setAddedUsers(selectedUsers);
+      }
     }
   };
 
   const okButtonText = addedUsers.length ? 'Add' : 'Done';
   const cancelButton = addedUsers.length ? (
-    <Button onClick={() => setAddedUsers([])}>Cancel</Button>) : null;
+    <Button onClick={() => setAddedUsers([])}>Cancel</Button>
+  ) : null;
 
   const inviteUsers = async () => {
     if (!addedUsers.length) return;
@@ -57,20 +75,47 @@ const ShareProjectModal = (props) => {
         role,
       },
     );
-
+    if (role === 'owner') {
+      dispatch(loadExperiments());
+    }
     onCancel();
   };
 
   return (
     <Modal
       open
-      title={[<UserAddOutlined />, 'Share with collaborators']}
+      title={[<UserAddOutlined key='icon' />, 'Share with collaborators']}
       onCancel={onCancel}
       okButtonText='Done'
       footer={(
         <Space direction='horizontal'>
           {cancelButton}
-          <Button onClick={() => inviteUsers()} type='primary'>{okButtonText}</Button>
+          {role === 'owner' ? (
+            <Popconfirm
+              title={() => (
+                <div>
+                  This action will transfer the ownership permissions to
+                  {' '}
+                  <b>{addedUsers[0]}</b>
+                  .
+                  {' '}
+                  <br />
+                  You will no longer have access to this project.
+                  {' '}
+                  <br />
+                  Are you sure you want to transfer owner permissions?
+                </div>
+              )}
+              onConfirm={inviteUsers}
+              okText='Yes'
+              cancelText='No'
+              disabled={!addedUsers.length}
+            >
+              <Button type='primary'>{okButtonText}</Button>
+            </Popconfirm>
+          ) : (
+            <Button onClick={() => inviteUsers()} type='primary'>{okButtonText}</Button>
+          )}
         </Space>
       )}
       width='650px'
@@ -80,7 +125,6 @@ const ShareProjectModal = (props) => {
           {project.name}
         </Text>
         <Row gutter={10} style={{ width: '110%' }}>
-
           <Col span={18}>
             <Select
               value={addedUsers}
@@ -93,17 +137,17 @@ const ShareProjectModal = (props) => {
           <Col span={6}>
             <Select defaultValue='explorer' onChange={(val) => setRole(val)}>
               <Select.Option key='explorer' value='explorer'> Explorer </Select.Option>
+              <Select.Option key='owner' value='owner' disabled={!canTransferOwnership}> Owner </Select.Option>
             </Select>
           </Col>
         </Row>
 
         <Row>
           <Space direction='vertical' style={{ width: '100%' }} size='large'>
-
             <Card key='users' style={{ width: '100%', height: '20rem', overflowY: 'auto' }}>
               {
                 usersWithAccess.map((user) => (
-                  <Row gutter={10}>
+                  <Row gutter={10} key={user.email}>
                     <Col span={3}>
                       <Avatar
                         style={{
