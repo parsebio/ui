ebd065cb29b18294727cc6a23dbce351a52e8f70
cosa65 <martin@biomage.net>
2025-04-10 14:38:14 -0300

    Add AuthorizedDisabler
    Signed-off-by: cosa65 <martin@biomage.net>

---
+++ src/utils/AuthorizedDisabler.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import { useSelector } from 'react-redux';
+import { getIsAuthorized } from 'redux/selectors';
+import { Tooltip } from 'antd';
+
+const scanpyDisableMessage = 'Your current role in this project does not allow you to perform this action.';
+
+const ScanpyDisabler = ({ experimentId, category, children }) => {
+  const isAuthorized = useSelector(getIsAuthorized(experimentId, category));
+
+  if (isAuthorized) {
+    return children;
+  }
+
+  // First div is to trigger hover events
+  // Second div is to disable everything under the tooltip without having to clone the children
+  return (
+    <Tooltip title={scanpyDisableMessage}>
+      <div>
+        <div disabled style={{ pointerEvents: 'none', opacity: 0.5 }}>
+          {children}
+        </div>
+      </div>
+    </Tooltip>
+  );
+};
+
+ScanpyDisabler.defaultProps = {
+  experimentId: undefined,
+};
+
+ScanpyDisabler.propTypes = {
+  children: PropTypes.node.isRequired,
+  category: PropTypes.string.isRequired,
+  experimentId: PropTypes.string,
+};
+
+export default ScanpyDisabler;
+
+export { scanpyDisableMessage };
