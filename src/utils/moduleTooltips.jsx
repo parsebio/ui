fc78c26b2afe4e27ec0e438fbf3a2be8b4d0da38
cosa65 <martin@biomage.net>
2025-05-05 15:01:56 -0300

    Add other tooltips

--- src/utils/moduleTooltips.jsx
+++ src/utils/moduleTooltips.jsx
@@ -30,6 +30,60 @@ const moduleTooltips = {
       (requires support suite login)
     </>
   ),
+  TERTIARY_PROJECT: (
+    <>
+      Processed data files are input for downstream filtering, integration and cell type annotation,
+      as well as deep dive visualization and exploration of gene expression and
+      differential expression.
+      A range of fully customizable plots enable the generation of publication-ready figures.
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/36215511478292-Guided-walkthrough-Insights-module-set-up' target='_blank' rel='noreferrer'>Guided Walkthrough: Insights module set-up</a>
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/27076682137236-Trailmaker-User-Guide#h_01HZ4VDNQ80HNKPV6CDAW8ZX16' target='_blank' rel='noreferrer'>User Guide - Insights module</a>
+      <br />
+      <br />
+    </>
+  ),
+  TERTIARY_DATA_PROCESSING: (
+    <>
+      View quality control plots and adjust settings for filtering, integration and clustering.
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/36215974172692-Guided-walkthrough-Insights-Data-Processing' target='_blank' rel='noreferrer'>Guided Walkthrough: Insights Data Processing</a>
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/27076682137236-Trailmaker-User-Guide#h_01HZ4VDNQ9D1X142H65V6GHQ22' target='_blank' rel='noreferrer'>User Guide - Data Processing section</a>
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/31741233000468-How-to-adjust-data-processing-settings-to-fit-your-dataset-and-troubleshoot-data-processing-failures' target='_blank' rel='noreferrer'>How to adjust data processing settings to fit your dataset and troubleshoot data processing failures</a>
+    </>
+  ),
+  TERTIARY_DATA_EXPLORATION: (
+    <>
+      Take a deep dive into your dataset: view marker heatmap and gene expression,
+      customize and annotate clusters, and perform differential expression and pathway analysis.
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/36216095150484-Guided-walkthrough-Insights-Data-Exploration' target='_blank' rel='noreferrer'>Guided Walkthrough: Insights Data Exploration</a>
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/27076682137236-Trailmaker-User-Guide#h_01HZ4VDNQAYF2CF8PEDVNAXA7B' target='_blank' rel='noreferrer'>User Guide - Data Exploration section</a>
+    </>
+  ),
+  TERTIARY_PLOTS_AND_TABLES: (
+    <>
+      A range of plot types are available to visualize your data, all of which can
+      be customized to your design preferences and exported as high resolution images.
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/36216493945364-Guided-walkthrough-Insights-Plots-and-Tables' target='_blank' rel='noreferrer'>Guided Walkthrough: Insights Plots and Tables</a>
+      <br />
+      <br />
+      <a href='https://support.parsebiosciences.com/hc/en-us/articles/27076682137236-Trailmaker-User-Guide#h_01HZ4VDNQBKY9VX7NF5XX1RTWT' target='_blank' rel='noreferrer'>User Guide - Plots and Tables section</a>
+    </>
+  ),
 };
 
 export default moduleTooltips;
