b474d8187d9a17463a7362f6b5c03f62332d4044
cosa65 <martin@biomage.net>
2025-05-26 10:27:46 -0300

    Comment before emrging

--- src/components/data-management/FileUploadModal.jsx
+++ src/components/data-management/FileUploadModal.jsx
@@ -195,8 +195,11 @@ const FileUploadModal = (props) => {
                   aria-label='sampleTechnologySelect'
                   data-testid='uploadTechSelect'
                   defaultValue={selectedTech}
-                  disabled={currentSelectedTech === sampleTech.SEURAT}
-                  // disabled={currentSelectedTech}
+                  // TODO to enable multitech selection
+                  // - uncomment this line
+                  // - remove the currently enabled disable line
+                  // disabled={currentSelectedTech === sampleTech.SEURAT}
+                  disabled={currentSelectedTech}
                   onChange={(value) => setSelectedTech(value)}
                   // Fix the width so that the dropdown doesn't change size when the value changes
                   style={{ width: 180 }}
