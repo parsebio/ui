cc2ae35fd0eb671bef2d9586d8da16daccbc98e0
cosa65 <martin@biomage.net>
2025-05-29 18:17:24 -0300

    Add axios

--- src/utils/http/fetchAPI.js
+++ src/utils/http/fetchAPI.js
@@ -4,6 +4,7 @@ import getApiEndpoint from 'utils/apiEndpoint';
 import getAuthJWT from 'utils/getAuthJWT';
 import { notAgreedToTermsStatus } from 'utils/constants';
 import { loadUser } from 'redux/actions/user';
+import axios from 'axios';
 
 let dispatch;
 
@@ -37,35 +38,31 @@ const fetchAPI = async (path, params = {}, extras = {}) => {
 
   let response;
   try {
-    response = await fetch(url, parameters);
+    response = await axios({
+      url,
+      method: params.method || 'GET',
+      headers: parameters.headers,
+      data: params.body,
+      // You can add more axios config here if needed
+    });
   } catch (e) {
-    // wrap fetch errors in custom error
+    // wrap axios errors in custom error
     throw new FetchError(e);
   }
-  if (!response.ok) {
-    let data;
-    try {
-      data = await response.json();
-    } catch (e) {
-      // if we can't get extra error info from the response we don't want to fail
-      // just return the error code, this happens in many tests
-      // where we mock a string response instead of proper json
-    }
-
+  // Axios puts the status on response.status and data on response.data
+  if (!response.status || response.status < 200 || response.status >= 300) {
+    const { data } = response;
     // If the user didn't agree to the terms of use,
     // Reload the user so that we trigger the TermsOfUseIntercept
     if (response.status === notAgreedToTermsStatus) {
       dispatch(loadUser());
     }
-
-    // data.message & data.errors follow error formatting defined in:
-    // HTTPError.v1.yaml
     throw new APIError(response.status, data?.message, data?.errors);
   }
 
   if (!parseJson) return response;
 
-  return await response.json();
+  return response.data;
 };
 
 export default fetchAPI;
