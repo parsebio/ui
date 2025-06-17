d62875c3184bf23ad4a546115f73442e5cc919f7
cosa65 <martin@biomage.net>
2025-06-17 10:24:31 -0300

    Rollback fetchAPI to original using fetch

--- src/utils/http/fetchAPI.js
+++ src/utils/http/fetchAPI.js
@@ -4,7 +4,6 @@ import getApiEndpoint from 'utils/apiEndpoint';
 import getAuthJWT from 'utils/getAuthJWT';
 import { notAgreedToTermsStatus } from 'utils/constants';
 import { loadUser } from 'redux/actions/user';
-import axios from 'axios';
 
 let dispatch;
 
@@ -17,57 +16,46 @@ const fetchAPI = async (path, params = {}, extras = {}) => {
   const authJWT = extras.jwt || await getAuthJWT();
   const parseJson = extras.parseJson ?? true;
 
-  console.log('authJWTDebug');
-  console.log(authJWT);
-
-  console.log('extrasDebug');
-  console.log(extras);
-
   const parameters = {
     ...params,
     headers: {
       ...headers,
-      ...(authJWT && { authorization: `Bearer ${authJWT}` }),
+      ...(authJWT && { Authorization: `Bearer ${authJWT}` }),
     },
   };
-
-  console.log('parametersPostDebug');
-  console.log(JSON.stringify(parameters));
 
   const url = getApiEndpoint(extras.uiUrl) + path;
 
   let response;
   try {
-    axios.interceptors.request.use((request) => {
-      console.log(`FullRequestDebug: ${JSON.stringify(request)}]}`);
-      return request;
-    });
-
-    response = await axios({
-      url,
-      method: params.method || 'GET',
-      headers: parameters.headers,
-      data: params.body,
-      // You can add more axios config here if needed
-    });
+    response = await fetch(url, parameters);
   } catch (e) {
-    // wrap axios errors in custom error
+    // wrap fetch errors in custom error
     throw new FetchError(e);
   }
   // Axios puts the status on response.status and data on response.data
-  if (!response.status || response.status < 200 || response.status >= 300) {
-    const { data } = response;
+  if (!response.ok) {
+    let data;
+    try {
+      data = await response.json();
+    } catch (e) {
+      // if we can't get extra error info from the response we don't want to fail
+      // just return the error code, this happens in many tests
+      // where we mock a string response instead of proper json
+    }
+
     // If the user didn't agree to the terms of use,
     // Reload the user so that we trigger the TermsOfUseIntercept
     if (response.status === notAgreedToTermsStatus) {
       dispatch(loadUser());
     }
+
     throw new APIError(response.status, data?.message, data?.errors);
   }
 
   if (!parseJson) return response;
 
-  return response.data;
+  return await response.json();
 };
 
 export default fetchAPI;
