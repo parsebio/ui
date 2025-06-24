b315090ee640edbc08fdff33feb71ef5c3c0e603
cosa65 <martin@biomage.net>
2025-06-24 14:26:40 -0300

    Fix

--- src/utils/signIn.js
+++ src/utils/signIn.js
@@ -1,15 +1,8 @@
 import { Auth } from '@aws-amplify/auth';
-import Cookies from 'js-cookie';
 
 import cache from 'utils/cache';
 
 const signIn = async () => {
-  // Clear old cookies that Amplify might have left behind.
-  // This avoids infinite redirect loops when the user tries to sign in
-  Object.keys(Cookies.get())
-    .filter((key) => key.startsWith('CognitoIdentityServiceProvider.'))
-    .forEach((key) => Cookies.remove(key));
-
   // Store current url so that post-login we can send them to the place they expected
   cache.set('redirectUrl', window.location.href);
   await Auth.federatedSignIn();
