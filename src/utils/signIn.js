49ffb873a645e2070ababade1a13c7391b938cb5
cosa65 <martin@biomage.net>
2025-06-25 13:16:41 -0300

    More logs

--- src/utils/signIn.js
+++ src/utils/signIn.js
@@ -4,11 +4,22 @@ import Cookies from 'js-cookie';
 import cache from 'utils/cache';
 
 const signIn = async () => {
+  const cookies = Cookies.get();
+  console.log('cookiesDebug');
+  console.log(cookies);
+
+  const cookiesKeys = Object.keys(cookies);
+  console.log('ObjectkeyscookiesKeysDebug');
+  Object.keys(cookiesKeys);
   // Clear old cookies that Amplify might have left behind.
   // This avoids infinite redirect loops when the user tries to sign in
-  Object.keys(Cookies.get())
-    .filter((key) => key.startsWith('CognitoIdentityServiceProvider.'))
-    .forEach((key) => Cookies.remove(key));
+  const filteredCookies = cookiesKeys
+    .filter((key) => key.startsWith('CognitoIdentityServiceProvider.'));
+
+  console.log('filteredCookiesDebug');
+  console.log(filteredCookies);
+
+  filteredCookies.forEach((key) => Cookies.remove(key));
 
   // Store current url so that post-login we can send them to the place they expected
   cache.set('redirectUrl', window.location.href);
