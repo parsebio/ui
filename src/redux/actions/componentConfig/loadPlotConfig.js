3be35d5f350aa45f9f9d2390f915662eb9437dbd
stefanbabukov <stefanbabukov98@gmail.com>
2025-06-26 12:11:49 +0100

    update plot data when qc is ran
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/redux/actions/componentConfig/loadPlotConfig.js
+++ src/redux/actions/componentConfig/loadPlotConfig.js
@@ -1,27 +1,38 @@
 import _ from 'lodash';
 import fetchAPI from 'utils/http/fetchAPI';
 import endUserMessages from 'utils/endUserMessages';
-import { LOAD_CONFIG } from 'redux/actionTypes/componentConfig';
+import { LOAD_CONFIG, PLOT_DATA_LOADED } from 'redux/actionTypes/componentConfig';
 import { initialPlotConfigStates } from 'redux/reducers/componentConfig/initialState';
 import handleError from 'utils/http/handleError';
 import httpStatusCodes from 'utils/http/httpStatusCodes';
 
-const loadPlotConfig = (experimentId, plotUuid, plotType) => async (dispatch) => {
+const loadPlotConfig = (experimentId, plotUuid, plotType = null) => async (dispatch) => {
   try {
     const data = await fetchAPI(`/v2/experiments/${experimentId}/plots/${plotUuid}`);
+    console.log('GOT NEW DATA FOR PLOT ', plotUuid, data);
 
-    const plotConfig = _.merge({}, initialPlotConfigStates[plotType], data.config);
-
-    dispatch({
-      type: LOAD_CONFIG,
-      payload: {
-        experimentId,
-        plotUuid,
-        plotType,
-        plotData: data.plotData,
-        config: plotConfig,
-      },
-    });
+    if (plotType) {
+      const plotConfig = _.merge({}, initialPlotConfigStates[plotType], data.config);
+      dispatch({
+        type: LOAD_CONFIG,
+        payload: {
+          experimentId,
+          plotUuid,
+          plotType,
+          plotData: data.plotData,
+          config: plotConfig,
+        },
+      });
+    } else {
+      dispatch({
+        type: PLOT_DATA_LOADED,
+        payload: {
+          experimentId,
+          plotUuid,
+          plotData: data.plotData,
+        },
+      });
+    }
   } catch (e) {
     // load default plot config if it not found
     if (e.statusCode === httpStatusCodes.NOT_FOUND) {
