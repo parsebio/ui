43d1760bbe5ce14be314bbc69e86243cb8360d3f
cosa65 <martin@biomage.net>
2025-07-15 14:45:28 -0300

    Add basic test

--- src/__test__/components/secondary-analysis/FastqPairsMatcher.test.jsx
+++ src/__test__/components/secondary-analysis/FastqPairsMatcher.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
@@ -13,14 +13,22 @@ import {
 } from 'redux/actions/secondaryAnalyses';
 import mockAPI, {
   generateDefaultMockAPIResponses,
+  promiseResponse,
 } from '__test__/test-utils/mockAPI';
 import { mockAnalysisIds } from '__test__/data/secondaryAnalyses/secondary_analyses';
 
 import FastqPairsMatcher from 'components/secondary-analysis/FastqPairsMatcher';
+import mockAnalysisFilesWithImmune from '__test__/data/secondaryAnalyses/secondary_analysis_files_with_immune';
 
 const readyToLaunchResponses = generateDefaultMockAPIResponses(mockAnalysisIds.readyToLaunch);
 const emptyAnalysisResponses = generateDefaultMockAPIResponses(mockAnalysisIds.emptyAnalysis);
-const mockAPIResponses = { ...emptyAnalysisResponses, ...readyToLaunchResponses };
+const mockAPIResponses = {
+  ...emptyAnalysisResponses,
+  ...readyToLaunchResponses,
+  [`/v2/secondaryAnalysis/${mockAnalysisIds.readyToLaunch}/files`]: () => promiseResponse(
+    JSON.stringify(mockAnalysisFilesWithImmune),
+  ),
+};
 
 enableFetchMocks();
 
@@ -60,5 +68,27 @@ describe('FastqPairsMatcher', () => {
 
     // Two empty selects for immune pairs
     expect(screen.getAllByText('Select immune pair')).toHaveLength(2);
+  });
+
+  it('Selecting functionality works fine', async () => {
+    await renderSelect();
+
+    // Open the first immune select dropdown and select the first immune pair
+    const selects = screen.getAllByRole('combobox');
+    fireEvent.mouseDown(selects[0]);
+    const tcrS1Option = screen.getAllByText('TCR_S1')[1];
+    fireEvent.click(tcrS1Option);
+
+    // The dropdown value should now be TCR_S1 (selected)
+    const tcrS1Selected = document.querySelector('.ant-select-selection-item[title="TCR_S1"]');
+    expect(tcrS1Selected).toBeInTheDocument();
+
+    // Open the second immune select dropdown and select the second immune pair
+    fireEvent.mouseDown(selects[1]);
+    const tcrS2Option = screen.getAllByText('TCR_S2')[1];
+    fireEvent.click(tcrS2Option);
+
+    const tcrS2Selected = document.querySelector('.ant-select-selection-item[title="TCR_S2"]');
+    expect(tcrS2Selected).toBeInTheDocument();
   });
 });
---
+++ src/__test__/data/secondaryAnalyses/secondary_analysis_files_with_immune.js
@@ -0,0 +1,97 @@
+const mockAnalysisFilesWithImmune = {
+  files: [
+    {
+      id: '1000fe51-6a6d-4545-b717-a4c475849b94',
+      name: 'S1_R1.fastq.gz',
+      size: '70154428',
+      type: 'wtFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:51.665893+00',
+    },
+    {
+      id: 'd7ba4d40-e1ed-45a0-9eca-8f5caab0dd7b',
+      name: 'S1_R2.fastq.gz',
+      size: '55879599',
+      type: 'wtFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:53.557915+00',
+    },
+    {
+      id: 'c28efb10-6459-4107-b9ef-baf6b13968c4',
+      name: 'S2_R1.fastq.gz',
+      size: '70151802',
+      type: 'wtFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:55.356064+00',
+    },
+    {
+      id: '0740a094-f020-4c97-aceb-d0a708d0982e',
+      name: 'S2_R2.fastq.gz',
+      size: '55879479',
+      type: 'wtFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:57.50098+00',
+    },
+    {
+      id: '1000fe51-6a6d-4545-b717-a4c475849b95',
+      name: 'TCR_S1_R1.fastq.gz',
+      size: '70154428',
+      type: 'immuneFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:51.665893+00',
+    },
+    {
+      id: 'd7ba4d40-e1ed-45a0-9eca-8f5caab0dd7c',
+      name: 'TCR_S1_R2.fastq.gz',
+      size: '55879599',
+      type: 'immuneFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:53.557915+00',
+    },
+    {
+      id: 'c28efb10-6459-4107-b9ef-baf6b13968c5',
+      name: 'TCR_S2_R1.fastq.gz',
+      size: '70151802',
+      type: 'immuneFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:55.356064+00',
+    },
+    {
+      id: '0740a094-f020-4c97-aceb-d0a708d0982f',
+      name: 'TCR_S2_R2.fastq.gz',
+      size: '55879479',
+      type: 'immuneFastq',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-04 15:29:57.50098+00',
+    },
+    {
+      id: 'a3e9f5f5-fbcb-46eb-be1f-bbeb9fe3491e',
+      name: 'pbmc_1Mreads sltab.xlsm',
+      size: '922034',
+      type: 'samplelt',
+      upload: {
+        status: 'uploaded',
+      },
+      createdAt: '2024-07-08 14:17:49.164486+00',
+    },
+  ],
+  pairMatches: [],
+};
+
+export default mockAnalysisFilesWithImmune;
