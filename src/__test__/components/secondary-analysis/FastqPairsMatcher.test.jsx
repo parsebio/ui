db0fa849c22f8aaf1d1f71e01b7354dd9761f133
cosa65 <martin@biomage.net>
2025-07-15 14:27:55 -0300

    Set up base test for FastqPairsMatcher

---
+++ src/__test__/components/secondary-analysis/FastqPairsMatcher.test.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import '@testing-library/jest-dom';
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
+import { Provider } from 'react-redux';
+import { makeStore } from 'redux/store';
+import {
+  loadSecondaryAnalyses,
+  loadSecondaryAnalysisFiles,
+  loadSecondaryAnalysisStatus,
+  setActiveSecondaryAnalysis,
+} from 'redux/actions/secondaryAnalyses';
+import mockAPI, {
+  generateDefaultMockAPIResponses,
+} from '__test__/test-utils/mockAPI';
+import { mockAnalysisIds } from '__test__/data/secondaryAnalyses/secondary_analyses';
+
+import FastqPairsMatcher from 'components/secondary-analysis/FastqPairsMatcher';
+
+const readyToLaunchResponses = generateDefaultMockAPIResponses(mockAnalysisIds.readyToLaunch);
+const emptyAnalysisResponses = generateDefaultMockAPIResponses(mockAnalysisIds.emptyAnalysis);
+const mockAPIResponses = { ...emptyAnalysisResponses, ...readyToLaunchResponses };
+
+enableFetchMocks();
+
+describe('FastqPairsMatcher', () => {
+  let storeState;
+
+  const renderSelect = async () => await render(
+    <Provider store={storeState}>
+      <FastqPairsMatcher />
+    </Provider>,
+  );
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    fetchMock.resetMocks();
+    fetchMock.mockIf(/.*/, mockAPI(mockAPIResponses));
+    storeState = makeStore();
+    await storeState.dispatch(loadSecondaryAnalyses());
+    await storeState.dispatch(loadSecondaryAnalysisFiles(mockAnalysisIds.readyToLaunch));
+    await storeState.dispatch(loadSecondaryAnalysisStatus(mockAnalysisIds.readyToLaunch));
+
+    await act(async () => {
+      await storeState.dispatch(setActiveSecondaryAnalysis(mockAnalysisIds.readyToLaunch));
+    });
+  });
+
+  it('Renders', async () => {
+    await renderSelect();
+  });
+});
