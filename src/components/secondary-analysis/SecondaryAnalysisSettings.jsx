1c583538abb9a23b9dd46fde0265377869ee6e77
stefanbabukov <stefanbabukov98@gmail.com>
2025-07-24 12:32:54 +0100

    change alert messages
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/secondary-analysis/SecondaryAnalysisSettings.jsx
+++ src/components/secondary-analysis/SecondaryAnalysisSettings.jsx
@@ -81,10 +81,9 @@ const SecondaryAnalysisSettings = (props) => {
       ) {
         Modal.confirm({
           title: 'You have immune profiling FASTQ files uploaded.',
-          content:
-            'If you switch back to a WT‐only kit, those files won’t be processed. Do you still want to continue? Your files will not be deleted.',
-          okText: 'Yes, switch kit',
-          cancelText: 'No, keep current kit',
+          content: 'By selecting a WT kit, your uploaded immune profiling files will not be processed.',
+          okText: 'OK',
+          cancelText: 'Cancel',
           onOk: () => changeKit(newKit),
         });
       } else {
@@ -201,8 +200,8 @@ const SecondaryAnalysisSettings = (props) => {
                 if (!value && Object.keys(wtFiles).length > 0) {
                   Modal.confirm({
                     title: 'You have WT Fastq files uploaded.',
-                    content: 'Do you want to disable paired WT mode? Your uploaded WT files will not be deleted, but they will be hidden and not used in your pipeline run.',
-                    okText: 'Yes, disable',
+                    content: 'By disabling this toggle, your uploaded WT files will not be processed.',
+                    okText: 'OK',
                     cancelText: 'Cancel',
                     onOk: () => handleValueChange('pairedWt', false),
                   });
