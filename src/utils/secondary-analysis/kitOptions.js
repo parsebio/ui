a42f2269b7a2cc88eb0e41f471337cab1830184f
cosa65 <martin@biomage.net>
2025-07-29 11:16:48 -0300

    Comment out immune kit options

--- src/utils/secondary-analysis/kitOptions.js
+++ src/utils/secondary-analysis/kitOptions.js
@@ -10,10 +10,10 @@ const kitOptions = [
   { label: 'Evercode WT Mega', value: 'wt_mega' },
   { label: 'Evercode WT Mega 384', value: 'wt_mega_384' },
   // { label: 'Evercode TCR Mini', value: 'tcr_mini' },
-  { label: 'Evercode TCR', value: 'tcr' },
+  // { label: 'Evercode TCR', value: 'tcr' },
   // { label: 'Evercode TCR Mega', value: 'tcr_mega' },
   // { label: 'Evercode BCR Mini', value: 'bcr_mini' },
-  { label: 'Evercode BCR', value: 'bcr' },
+  // { label: 'Evercode BCR', value: 'bcr' },
   // { label: 'Evercode BCR Mega', value: 'bcr_mega' },
 ];
 
