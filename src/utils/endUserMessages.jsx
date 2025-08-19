d5b89cbe31ea3022f91315022e760a8bffbef768
sara castellano <sara@biomage.net>
2025-08-19 11:03:46 +0200

    generalize annot error message
    Signed-off-by: sara castellano <sara@biomage.net>

--- src/utils/endUserMessages.jsx
+++ src/utils/endUserMessages.jsx
@@ -82,6 +82,6 @@ export default {
   ERROR_LAUNCHING_SECONDARY_ANALYSIS: 'There was a problem launching the pipeline.',
   ERROR_FASTQS_INCORRECTLY_PAIRED: 'Some fastqs are missing their corresponding read pairs.',
   ERROR_CANCELLING_SECONDARY_ANALYSIS: 'There was a problem cancelling your analysis.',
-  ERROR_CELL_SETS_ANNOTATION_FAILED: 'ScType annotation unsuccessful. Please ensure dataset contains gene symbols.',
+  ERROR_CELL_SETS_ANNOTATION_FAILED: 'Clusters annotation unsuccessful. Please ensure dataset contains gene symbols.',
   ERROR_UPDATING_FASTQ_PAIRS: 'Error updating Fastq pair matches',
 };
