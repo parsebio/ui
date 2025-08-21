a027d7e84fb5732a97378f12c26c75d633f2df6b
sara castellano <sara@biomage.net>
2025-08-21 09:01:30 +0200

    fix lint
    Signed-off-by: sara castellano <sara@biomage.net>

--- src/redux/actions/cellSets/loadCellSets.js
+++ src/redux/actions/cellSets/loadCellSets.js
@@ -31,9 +31,8 @@ const loadCellSets = (experimentId, forceReload = false) => async (dispatch, get
     const cellSetsData = await downloadFromS3('CellSets', signedUrl);
 
     // Debug: flag any cell sets containing non-numeric IDs
-    const badSets = Object.entries(cellSetsData.cellSets.properties ?? {})
-      .filter(([_, { cellIds }]) => Array.from(cellIds || [])
-        .some((id) => !Number.isInteger(id)));
+    const badSets = Object.entries(cellSetsData.cellSets?.properties ?? {})
+      .filter(([, { cellIds } = {}]) => Array.from(cellIds ?? []).some((id) => !Number.isInteger(id)));
     console.log(badSets.length ? badSets : '*** All cellIds are numeric');
 
     dispatch({
