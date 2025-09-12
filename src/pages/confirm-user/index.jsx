a7047d191b6fae7d7dfbd167759cb567387e7ef6
cosa65 <martin@biomage.net>
2025-09-12 10:24:03 -0300

    Update

--- src/pages/confirm-user/index.jsx
+++ src/pages/confirm-user/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Auth } from '@aws-amplify/auth';
 
-const confirmUserPage = () => (
-  <div>confirmUserPage</div>
-);
+const userMessages = {
+  // CodeMismatchException: 'Invalid code provided, please try again.',
+  expiredCode: 'This verification code has expired, it might be because your account is already confirmed. If not, please request a new code.',
+  userAlreadyConfirmed: 'This user is already confirmed!',
+  confirmedSuccessfully: 'Your account has been confirmed successfully! You can now log in.',
+};
+
+const confirmUserPage = () => {
+  const [statusMessage, setStatusMessage] = useState('');
+
+  const confirmSignup = useCallback(async () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const username = searchParams.get('user_name');
+    const code = searchParams.get('confirmation_code');
+
+    try {
+      await Auth.confirmSignUp(username, code);
+      setStatusMessage(userMessages.confirmedSuccessfully);
+    } catch (e) {
+      console.error('errorDebug');
+      console.error(e);
+
+      if (e.code === 'ExpiredCodeException') {
+        setStatusMessage(userMessages.expiredCode);
+      }
+
+      if (e.code === 'NotAuthorizedException' && e.message === 'User cannot be confirmed. Current status is CONFIRMED') {
+        setStatusMessage(userMessages.userAlreadyConfirmed);
+      }
+
+      setStatusMessage(e.message);
+    }
+  });
+
+  confirmSignup();
+
+  return <div>{statusMessage}</div>;
+};
 
 export default confirmUserPage;
